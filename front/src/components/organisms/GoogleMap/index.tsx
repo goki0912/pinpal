ea23d0e9d073a2123be5074754f70b6bfb1cfaf3
kazuki1023 <k.keio12569@gmail.com>
2024-02-10 18:57:38 +0900

    markerが表示されるようになった

--- front/src/components/organisms/GoogleMap/index.tsx
+++ front/src/components/organisms/GoogleMap/index.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useRef, useState } from 'react';
 const INITIALIZE_LAT = 35.68238;
 const INITIALIZE_LNG = 139.76556;
 const INITIALIZE_ZOOM = 16; // ズームレベル
+const MAPID = process.env.NEXT_PUBLIC_GOOGLE_MAP_API_ID;
 
 
 const GoogleMap: React.FC = () => {
@@ -38,9 +39,31 @@ const GoogleMap: React.FC = () => {
             zoomControl: false,
             streetViewControl: false,
             mapTypeControl: false,
+            mapId: MAPID,
         });
 
         setMap(initializedMap);
+
+        const markers = [
+            { lat: 35.68238, lng: 139.76556, content: 'Marker 1', color: { r: 255, g: 0, b: 0, a: 1.0 } },
+            { lat: 35.64238, lng: 139.76565, content: 'Marker 2', color: { r: 0, g: 255, b: 0, a: 1.0 } },
+            { lat: 35.68238, lng: 139.76543, content: 'Marker 3', color: { r: 11, g: 24, b: 46, a: 1.0 } },
+        ];
+
+        markers.forEach(markerInfo => {
+            const marker = new google.maps.Marker({
+                position: { lat: markerInfo.lat, lng: markerInfo.lng },
+                map: initializedMap,
+                title: markerInfo.content,
+                icon: {
+                    path: google.maps.SymbolPath.CIRCLE,
+                    fillColor: `rgba(${markerInfo.color.r},${markerInfo.color.g},${markerInfo.color.b},${markerInfo.color.a})`,
+                    fillOpacity: 1.0,
+                    strokeWeight: 0,
+                    scale: 8, 
+                },
+            });
+        });
     }, []);
 
     return (
