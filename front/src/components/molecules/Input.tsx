65851b4e95a91e5dac12a4682c8fe3ef6f2df491
kazuki1023 <k.keio12569@gmail.com>
2024-02-11 03:29:07 +0900

    名前と日付が送信できるようになった

--- front/src/components/molecules/Input.tsx
+++ front/src/components/molecules/Input.tsx
@@ -1,21 +1,21 @@
 interface InputProps {
   name: string;
-  form_name: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>)=>void;
+  [key: string]: any;
 }
 
-const Input: React.FC<InputProps> = ({name,form_name, onChange}) => {
+const Input: React.FC<InputProps> = ({name, register, ...rest}) => {
+  console.log(rest);
   return (
     <div className="m-5">
         <label className="block mb-2 text-sm font-medium text-gray-900">{name}</label>
       <input 
       type="text" 
       id="small-input" 
-      name={form_name} 
+      name={name} 
+      {...register(name)}
       className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500"
-      onChange={(e)=>onChange(e)}
-      >
-      </input>
+      {...rest}
+      />
     </div>
   );
 };
--- front/src/components/organisms/CreatePlaceForm.tsx
+++ front/src/components/organisms/CreatePlaceForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import DatePicker from '../molecules/DatePicker';
+// import DatePicker from '../molecules/DatePicker';
 import CreatePlace from '../molecules/CreatePlace';
 import Title from '../molecules/Title';
 import Input from '../molecules/Input';
@@ -8,16 +8,21 @@ import GroupMenu from '../molecules/GroupMenu';
 import Button from '../atoms/Button';
 import { useGroups } from '@/hooks/useGroups';
 import Autocomplete from '@/components/molecules/AutoComplete';
-import { useForm } from "react-hook-form";
-import { useCreatePlace } from '@/hooks/useCreatePlaces';
+import { useForm, Controller } from "react-hook-form";
+// import { useCreatePlace } from '@/hooks/useCreatePlaces';
+import DatePicker from "react-multi-date-picker";
+import DatePanel from "react-multi-date-picker/plugins/date_panel";
+import { PlacesPost } from '@/types/placesPost';
+import { createPlace } from '@/pages/api/places';
 
 
 interface CreatePlaceProps {
   onClick: () => void;
 }
 
-const CreatePlaceForm: React.FC<CreatePlaceProps> = ({onClick}) => { 
-  const groups  = useGroups();
+const CreatePlaceForm: React.FC<CreatePlaceProps> = ({ onClick }) => {
+  const groups = useGroups();
+  const { control, handleSubmit, getValues, setValue, register } = useForm<PlacesPost>();
 
   const [location, setLocation] = useState({ lat: 0, lng: 0 });
   console.log(location);
@@ -26,37 +31,55 @@ const CreatePlaceForm: React.FC<CreatePlaceProps> = ({onClick}) => {
     setLocation({ lat, lng });
   };
 
-  const {
-    handleChange, 
-    handleSubmitCreate,
-  } = useCreatePlace();
+  const onSubmit = async (data: any) => {
+    // getValuesを使用して、react-hook-formから最新のフォームデータを取得
+    const formData = getValues();
+    console.log(formData);
+    await createPlace(formData); 
+  };
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors }
-  } = useForm();
-  
+
+  const handleDateChange = (dates: any) => {
+    dates = dates.map((date: any) => date.format("YYYY-MM-DD"));
+    setValue('date', dates);
+  };
+  console.log(register('name'));
+
   return (
-      <div>
-        <div className="flex items-center justify-between p-3">
-          <Title title="Places I Want to Go" subtitle="行きたい場所をストックしよう！" />
-        </div>     
-        <hr></hr>
-        <form onSubmit={handleSubmit(handleSubmitCreate)}>
-          {/* ok */}
-          <Input name="場所：Place name" form_name="name" onChange={handleChange}/> 
-          {/* no */}
-          <Autocomplete name="住所：Place address" form_name="address" onSelectPlace={handleSelectPlace} />
-          {/* ok */}
-          <GroupMenu groups={groups} onChange={handleChange}/>
-          <MailCheckbox onClick={()=>console.log("aaa")}/>
-          {/* no */}
-          <DatePicker />
-          <div className="w-full flex justify-center">
-            <Button onClick={onClick} title="決定"/>
-          </div>
-        </form>
+    <div>
+      <div className="flex items-center justify-between p-3">
+        <Title title="Places I Want to Go" subtitle="行きたい場所をストックしよう！" />
+      </div>
+      <hr></hr>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* ok */}
+        <Input {...register('name')} register={register} />
+        {/* no */}
+        <Autocomplete name="住所：Place address" form_name="address" onSelectPlace={handleSelectPlace} />
+        {/* ok */}
+        {/* <GroupMenu groups={groups} onChange={handleChange}/> */}
+        <MailCheckbox onClick={() => console.log("aaa")} />
+        {/* no */}
+        {/* <DatePicker /> */}
+        <Controller
+        name="date" 
+        control={control} 
+        render={({
+          field: { onChange, name, value },
+        }) => (
+          <DatePicker 
+            value={value}
+            onChange={handleDateChange}
+            multiple 
+            inputClass="custom-input-size" 
+            plugins={[<DatePanel />]} 
+          />
+        )}
+      />
+        <div className="w-full flex justify-center">
+          <Button onClick={onClick} title="決定" />
+        </div>
+      </form>
     </div>
   );
 }
