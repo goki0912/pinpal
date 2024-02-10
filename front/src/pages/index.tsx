5f1c8205834150e8b9aefcf7fc17ee30648ba6f6
Yuma-Tsukakoshi <yttn9984kawa@gmail.com>
2024-02-10 20:13:40 +0900

    del: テストコード削除

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,12 +1,8 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { useGroups } from "@/hooks/useGroups";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const groups = useGroups();
-  console.log(groups);
-  
   return (
     <main>
     </main>
