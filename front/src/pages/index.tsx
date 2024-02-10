6a2b8bc70088a48c09cafca89c7598f50bcc382b
Yuma-Tsukakoshi <yttn9984kawa@gmail.com>
2024-02-10 18:10:30 +0900

    add: バックエンドと接続完了

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,11 +1,24 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { usePlaces } from "@/hooks/usePlaces";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const places = usePlaces();
+  console.log(places);
+
   return (
     <main>
+      <h1>Places</h1>
+      <ul>
+        {places?.map((place) => (
+          <li key={place.id}>
+            
+            <h2>{place.name}</h2>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
