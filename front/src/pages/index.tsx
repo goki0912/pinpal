e0c486bc6814d641190e001fb15a0a1249d6bb97
Yuma-Tsukakoshi <yttn9984kawa@gmail.com>
2024-02-10 19:50:32 +0900

    add: コメント削除

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,13 +1,9 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import  { usePlacesByGroup } from "@/hooks/usePlacesByGroup";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-
-  const places = usePlacesByGroup(1);
-  console.log(places);
 
   return (
     <main>
