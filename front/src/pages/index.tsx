44d02d6107e27962aa49a5f00f4abde014fc4950
uenoarisa <arisa-choco0326@docomo.ne.jp>
2024-02-10 13:26:13 +0900

    del: 使わないimport文削除

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,8 +1,5 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import Checkbox from "../components/atoms/Checkbox";
-import Button from "../components/atoms/Button";
-import MenuButton from "../components/atoms/MenuButton";
 
 const inter = Inter({ subsets: ["latin"] });
 
