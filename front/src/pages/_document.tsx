a9f1e6a9a1be144ba6411c69c9675399473a270e
kazuki1023 <k.keio12569@gmail.com>
2024-02-10 23:19:26 +0900

    autoComplete用のapiを読み込ませた

--- front/src/pages/_document.tsx
+++ front/src/pages/_document.tsx
@@ -8,10 +8,17 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     {googleMapsApiKey && (
-                        <script
-                            async
-                            src={`https://maps.googleapis.com/maps/api/js?key=${googleMapsApiKey}&v=beta&libraries=marker`}
-                        />
+                        <>
+                            <script
+                                async
+                                src={`https://maps.googleapis.com/maps/api/js?key=${googleMapsApiKey}&v=beta&libraries=marker`}
+                            />
+                            <script
+                                src={`https://maps.googleapis.com/maps/api/js?key=${googleMapsApiKey}&libraries=places`}
+                                async
+                                id="google-maps"
+                            ></script>
+                        </>
                     )}
                 </Head>
                 <body>
