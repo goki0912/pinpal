bc23d8808c3cea23b511dcfc812ee6570f0ba669
kazuki1023 <k.keio12569@gmail.com>
2024-02-10 22:08:47 +0900

    placeの型を変更

--- front/src/hooks/usePlaces.tsx
+++ front/src/hooks/usePlaces.tsx
@@ -1,9 +1,9 @@
 import { getPlaces } from '@/pages/api/places';
 import React, { useState , useEffect } from "react";
-import { Places } from '@/types/places';   
+import { Place } from '@/types/place';   
 
 export const usePlaces = () => {
-    const [ places, setPlaces ] = useState<Places[]>([]);
+    const [ places, setPlaces ] = useState<Place[]>([]);
     const fetchPlaces = async () => {
         const Places = await getPlaces();
         setPlaces(Places);
