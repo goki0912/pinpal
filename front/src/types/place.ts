b05c4ec582879df8e5ab2a571bafa811da94736d
uenoarisa <arisa-choco0326@docomo.ne.jp>
2024-02-10 19:36:46 +0900

    fix: placeの型定義をAPIの通りに変更した

--- front/src/types/place.ts
+++ front/src/types/place.ts
@@ -1,9 +1,11 @@
+import { Group } from './group';
+
 export type Place = {
     id: number;
     name: string;
     status: number;
-    group_id: number;
+    group_id: Group[];
     latitude: number;
     longitude: number;
-    date: string | null;
+    date: string[];
 };
