a2f5e990f4b0dcddd50b6daa3182adce98b19fef
kazuki1023 <k.keio12569@gmail.com>
2024-02-11 01:39:51 +0900

    modalが表示されるようになった

--- front/src/pages/_document.tsx
+++ front/src/pages/_document.tsx
@@ -21,6 +21,7 @@ class MyDocument extends Document {
                     )}
                 </Head>
                 <body>
+                    <div id="modal-root"></div>
                     <Main />
                     <NextScript />
                 </body>
