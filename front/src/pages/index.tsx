b512391201f505a6696d1a154c9aa13ce7062f25
Yuma-Tsukakoshi <yttn9984kawa@gmail.com>
2024-02-10 19:10:54 +0900

    add:index.tsのテストコード削除

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,23 +1,12 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { usePlaces } from "@/hooks/usePlaces";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const places = usePlaces();
-  console.log(places);
 
   return (
     <main>
-      <h1>Places</h1>
-      <ul>
-        {places?.map((place) => (
-          <li key={place.id}>
-            
-          </li>
-        ))}
-      </ul>
     </main>
   );
 }
