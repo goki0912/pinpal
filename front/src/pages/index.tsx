cc4f41318d8868fb255707ca62399881ae12b21f
kazuki1023 <k.keio12569@gmail.com>
2024-02-10 21:42:12 +0900

    googleMapを表示させた

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import GoogleMap from '@/components/organisms/GoogleMap';
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
 
   return (
     <main>
+      <GoogleMap />
     </main>
   );
 }
