732b5a3a827e5267ea73bd5d8ddfd21b99b5701e
Goki Kanno <goki.kanno@gmail.com>
2024-02-10 22:04:02 +0900

    グループ作成画面作成

---
+++ front/src/components/molecules/InputGroupName.tsx
@@ -0,0 +1,13 @@
+interface InputGroupNameProps {
+  name: string;
+}
+
+const InputGroupName: React.FC<InputGroupNameProps> = ({name}) => {
+  return (
+    <div className="mb-5 w-full">
+      <input type="text" id="small-input" name={name} className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500" placeholder="グループ名"></input>
+    </div>
+  );
+}
+
+export default InputGroupName;
