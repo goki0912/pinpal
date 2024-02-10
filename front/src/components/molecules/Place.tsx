5e0f00eab2d6f317c08a88992cad251137728e32
uenoarisa <arisa-choco0326@docomo.ne.jp>
2024-02-10 14:58:50 +0900

    add: moleculeのPlaceコンポーネント作成

---
+++ front/src/components/molecules/Place.tsx
@@ -0,0 +1,34 @@
+import Checkbox from "../atoms/Checkbox";
+import LocationIcon from "../atoms/LocationIcon";
+import {Place} from "../../types/place";
+import {Group} from "../../types/group";
+
+interface PlaceProps {
+    place: Place;
+    group: Group
+    onClick: () => void;
+    // 確認モーダル表示
+    onMove: () => void; 
+    // mapに移動する関数を追加
+}
+
+const Place: React.FC<PlaceProps> = ({place, group,onClick,onMove}) => {
+  return (
+    <div className="flex items-center text-white h-1 m-2 p-8 rounded-lg" style={{backgroundColor:group.color}} onClick={onMove}>
+      <div className="mr-2 w-1/6">
+        <Checkbox onClick={onClick} color="#FFFFF" />
+      </div>
+      <div className="w-5/6 items-center">
+        <div className="flex">
+            <LocationIcon />
+            <span className="ml-3 text-xl font-medium">{place.name}</span>
+        </div>
+        <div className="w-full flex items-center">
+            <span className="text-sm px-12">{group.name}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Place;
