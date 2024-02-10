689aef72463bf1a7dc06e8ddd59ac56a74d2aefc
Yuma-Tsukakoshi <yttn9984kawa@gmail.com>
2024-02-10 14:25:15 +0900

    add: 特定のidによるグループ取得 実装

--- front/src/hooks/useGroups.tsx
+++ front/src/hooks/useGroups.tsx
@@ -2,7 +2,7 @@ import { getGroups } from '@/pages/api/groups';
 import React, { useState , useEffect } from "react";
 import { Groups } from '@/types/groups';
 
-export const usePlaces = () => {
+export const useGroups = () => {
     const [ groups, setGroups ] = useState<Groups[]>([]);
     const fetchGroups = async () => {
         const groups = await getGroups();
