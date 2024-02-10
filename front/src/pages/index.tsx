66aec537b173e0d0b284c13ac0b20930b912dba4
Goki Kanno <goki.kanno@gmail.com>
2024-02-10 19:29:54 +0900

    達成済みにしますか？というアラートを作成

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -2,12 +2,13 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Date from "../components/molecules/Date";
 import Email from "../components/molecules/Email";
+import PlaceAlert from "../components/molecules/PlaceAlert";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main>
-
+      <PlaceAlert place="Tokyo" onClick={() => {}} />
 
     </main>
   );
