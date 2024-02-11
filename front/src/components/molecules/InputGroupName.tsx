b6a4ba8c598afe257cf4ff3f995903648152a8dc
Yuma-Tsukakoshi <yttn9984kawa@gmail.com>
2024-02-11 14:53:51 +0900

    不要valueを削除

--- front/src/components/molecules/InputGroupName.tsx
+++ front/src/components/molecules/InputGroupName.tsx
@@ -5,7 +5,7 @@ interface InputGroupNameProps {
 const InputGroupName: React.FC<InputGroupNameProps> = ({name}) => {
   return (
     <div className="w-full">
-      <input type="text" id="small-input" name={name} className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500" placeholder="グループ名" value={"高校の友達"}></input>
+      <input type="text" id="small-input" name={name} className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500" placeholder="グループ名" ></input>
       {/* 更新の場合はvalueに元のグループ名が入る。新規作成の場合はvalueはなし */}
     </div>
   );
