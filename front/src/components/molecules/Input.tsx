d7de3f6a33ca2c35c09f06514d49b0300d504c9d
kazuki1023 <k.keio12569@gmail.com>
2024-02-11 09:21:51 +0900

    ref要素を追加

--- front/src/components/molecules/Input.tsx
+++ front/src/components/molecules/Input.tsx
@@ -3,14 +3,15 @@ interface InputProps {
   [key: string]: any;
 }
 
-const Input: React.FC<InputProps> = ({name, register, ...rest}) => {
+const Input: React.FC<InputProps> = ({name, register, ref ,...rest}) => {
   return (
-    <div className="m-5">
+    <div className="m-5" >
         <label className="block mb-2 text-sm font-medium text-gray-900">{name}</label>
       <input 
       type="text" 
       id="small-input" 
       name={name} 
+      ref={ref}
       {...register(name)}
       className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500"
       {...rest}
