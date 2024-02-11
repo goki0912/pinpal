50dbf26337c835138fb1ae8dfb7e3d60b60da2e0
Yuma-Tsukakoshi <yttn9984kawa@gmail.com>
2024-02-11 14:54:10 +0900

    buttonの上下を変更した

--- front/src/components/organisms/CreatePlaceForm.tsx
+++ front/src/components/organisms/CreatePlaceForm.tsx
@@ -100,8 +100,8 @@ const CreatePlaceForm: React.FC<CreatePlaceProps> = ({ changeVisible }) => {
           />
         )}
         <div className="w-full flex justify-center gap-x-7 px-6">
-          <Button title="決定" />
           <button type="button" className='text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 w-2/5' onClick={changeVisible}>キャンセル</button>
+          <Button title="決定" />
         </div>
       </form>
     </div>
